8c57ec16061b546255372865570458d76c9fdf22
DonDebo37 <adebolaadesanya54@gmail.com>
2025-01-11 10:37:09 +0000

    modified Vpc-cdk-project-stack

--- lib/vpc-cdk-project-stack.ts
+++ lib/vpc-cdk-project-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
 export class VpcCdkProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24
+        },
+        {
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24
+        }
+      ]
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'VpcCdkProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, 'VpcId',{
+      value: vpc.vpcId,
+      description: 'VPC ID'
+    })
   }
 }
