cfea52628666226504b287b039768b4254088db8
DonDebo37 <adebolaadesanya54@gmail.com>
2025-01-11 12:43:12 +0000

    update vpc-cdk-project

--- lib/vpc-cdk-project-stack.ts
+++ lib/vpc-cdk-project-stack.ts
@@ -3,10 +3,12 @@ import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 
 export class VpcCdkProjectStack extends cdk.Stack {
+  public readonly vpc: ec2.Vpc
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'MyVpc', {
+    this.vpc = new ec2.Vpc(this, 'MyVpc', {
       maxAzs: 2,
       subnetConfiguration: [
         {
@@ -18,12 +20,17 @@ export class VpcCdkProjectStack extends cdk.Stack {
           name: 'private',
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
           cidrMask: 24
+        },
+        {
+          name: "Database",
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24
         }
       ]
     })
 
     new cdk.CfnOutput(this, 'VpcId',{
-      value: vpc.vpcId,
+      value: this.vpc.vpcId,
       description: 'VPC ID'
     })
   }
